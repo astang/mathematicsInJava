06d0aa15d08ec95c2d7ff5ad0594062c3ac7b9a4
Alice Stang <alice@stacom.de>
2019-02-03 15:13:03 +0100

    added factorial recursive

--- src/Fac.java
+++ src/Fac.java
@@ -12,10 +12,32 @@ public class Fac {
         return result;
     }
 
+    static int recfac (int n) {
+        int result = 0;
+        if (n == 0) {
+            result = 1;
+            return result;
+        } else if (n == 1) {
+            return 1;
+        } else if (n > 1) {
+            result = (n * recfac(n - 1));
+            return result;
+        }
+        return result;
+    }
+
     public static void main (String [] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("n = ");
         Integer n = scanner.nextInt();
-        System.out.println( n + "! = " + fac(n));
+
+        if(n<0) {
+            System.out.println(n + "! = calculation of negative factorial not possible" );
+        } else {
+            System.out.println("factorial calculation: ");
+            System.out.println( n + "! = " + fac(n));
+            System.out.println("recursive factorial calculation: ");
+            System.out.println(n + "! = " + recfac(n));
+        }
     }
 }
